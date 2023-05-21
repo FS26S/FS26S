ca0809449f0ec23ccfe712c0f9d69a6c28a9cb32
camila-dv@hotmail.com <Am@r0813>
2023-05-21 15:32:31 -0300

    tela de login

---
+++ pages/login.js
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import LoginCard from "../src/components/loginCard/loginCard"
+import styles from '../styles/login.module.css'
+import Input from "../src/components/input/input";
+import Button from "../src/components/button/button";
+import Image from "next/image";
+export default function LoginPage(){
+    return (
+        <div className={styles.background}>
+
+            <LoginCard title="Controle de Patrimonio Reserva de Salas">
+                <hr className={styles.separador}/>
+                <h4 className={styles.utfpr}>UTFPR</h4>
+                <form className={styles.form}>
+                    <Input type="email" placeholder="Seu e-mail"/>
+                    <Input type="password" placeholder="Sua senha"/>
+                    <Link href={"/recuperaSenha"} className={styles.recuperarSenha}>Esqueci a Senha</Link>
+                    <Button>Entrar</Button>
+                    <Link href={"/cadastro"} className={styles.cadastro}>Cadastre-se</Link>
+                </form>
+            </LoginCard>
+        </div>
+    )
+}
---
+++ src/components/loginCard/loginCard.js
@@ -0,0 +1,10 @@
+import styles from './loginCard.module.css'
+
+export default function LoginCard({title, children}){
+    return(
+        <div className={styles.card}>
+            <h4 className={styles.title}>{title}</h4>
+            {children}
+        </div>
+    )
+}
